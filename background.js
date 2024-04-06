9b5ce69778eb5fa9ea72c53841f155f630f092e3
Jono997 <42048866+Jono997@users.noreply.github.com>
2024-04-06 11:59:08 +1100

    Added change background button

---
+++ background.js
@@ -0,0 +1,39 @@
+window.current_background = undefined;
+
+window.change_backround = function() {
+    debugger;
+    var backgrounds = [
+        ['1.png', 'https://tumblr.com/0000stuff', '0000stuff'],
+        ['2.png', 'https://tumblr.com/0000stuff', '0000stuff'],
+        ['3.png', 'https://tumblr.com/0000stuff', '0000stuff'],
+        ['4.jpg', 'https://tumblr.com/jumpcat7', 'JumpCat'],
+        ['5.png', 'https://twitter.com/kelinci_bukit', 'kris']
+    ];
+
+    var background = undefined;
+    while (background === undefined || background[0] === current_background)
+        background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+    current_background = background[0];
+    var bg_element = document.getElementById('background');
+    var bg_path = `backgrounds/${background[0]}`;
+    bg_element.style.backgroundImage = `url(${bg_path})`;
+    var bg_credit = document.getElementById('bg-credit');
+    bg_credit.setAttribute('href', background[1]);
+    bg_credit.innerHTML = background[2];
+
+    bg_element.style.transitionDuration = "0s";
+    bg_element.style.opacity = 0;
+    var img = new Image();
+    img.onload = function()
+    {
+        bg_element.style.transitionDuration = "1.5s";
+        bg_element.style.opacity = 1;
+    };
+    img.src = bg_path;
+};
+
+(function() {
+    var change_bg_button = document.getElementById("change_bg_button");
+    change_bg_button.addEventListener("click", change_backround);
+})();
+change_backround();
