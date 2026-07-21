897b36645428b5b7b491e6dad0d7092dd358a74c
Jono99 <42048866+Jono997@users.noreply.github.com>
2026-07-21 21:37:45 +1000

    Added theme setting to menu (non-functional)

--- common.js
+++ common.js
@@ -1,14 +1,70 @@
-// Initialisation stuff
-(function() {
-    var settings_menu = document.getElementById("settings-menu");
-    settings_menu.innerHTML = "SETTINGS THINGS GO HERE"
-})();
-
-function toggleSettingsMenu() {
-    var container_classes = document.getElementById("settings-container").classList;
-    const open_class = "open";
-    if (container_classes.contains(open_class))
-        container_classes.remove("open");
+// Adds a class to an element, or removes that class if the element already has it
+// Returns if the element has the class once toggled
+function toggleClass(element, _class)
+{
+    if (element.classList.contains(_class))
+        element.classList.remove(_class);
     else
-        container_classes.add("open");
+        element.classList.add(_class);
+    return element.classList.contains(_class);
 }
+
+// Opens/closes the settings menu
+function toggleSettingsMenu()
+{
+    toggleClass(document.getElementById("settings-container"), "open");
+}
+
+// Initialises the given dropdown element in the DOM
+function setupDropdown(dropdown)
+{
+    var on_change = dropdown.attributes['ondropdownchange'];
+    var button = undefined;
+    
+    for (child of dropdown.children)
+    {
+        if (child.classList.contains("dropdown-button"))
+        {
+            button = child;
+            child.addEventListener("click", (e) => {
+                document.active_dropdown = (toggleClass(dropdown, "active") ? dropdown : undefined);
+                e.stopPropagation();
+            });
+        }
+        
+        if (child.classList.contains("dropdown-options"))
+            for (option of child.children)
+            {
+                option.addEventListener("click", (e) => {
+                    var option = e.currentTarget;
+                    dropdown.attributes.setNamedItem(option.attributes['dropdown-value'].cloneNode());
+                    button.innerHTML = option.innerHTML;
+                    var on_change = dropdown.attributes['ondropdownchange'];
+                    if (on_change != null)
+                        (window[on_change.value])(dropdown.attributes['dropdown-id'].value, dropdown.attributes['dropdown-value'].value);
+                    e.stopPropagation();
+                });
+            }
+    }
+}
+
+// Alerts the current id and value of a dropdown. used for debugging.
+function alertDropdownValue(dropdown_id, dropdown_value)
+{
+    alert(`ID: ${dropdown_id}\nValue: ${dropdown_value}`);
+}
+
+// Initialise dropdowns
+(function() {
+    document.active_dropdown = undefined;
+    document.addEventListener("click", () => {
+        if (document.active_dropdown !== undefined)
+        {
+            document.active_dropdown.classList.remove("active");
+            document.active_dropdown = undefined;
+        }
+    });
+
+    for (dropdown of document.getElementsByClassName("dropdown"))
+        setupDropdown(dropdown);
+})();
