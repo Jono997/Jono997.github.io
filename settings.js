b640ed502f738bd3f6d01ac8487326d43bb41ade
Jono99 <42048866+Jono997@users.noreply.github.com>
2026-07-24 00:39:40 +1000

    Added Font Size and Page Width settings

--- settings.js
+++ settings.js
@@ -9,8 +9,31 @@
                 default: 0,
                 update: function(value) {
                     var css = document.documentElement.style;
-                    css.setProperty('--theme-bg-colour', window.j99_themes[value].bg_colour);
-                    css.setProperty('--theme-text-colour', window.j99_themes[value].text_colour);
+                    css.setProperty('--theme-bg-colour', j99_themes[value].bg_colour);
+                    css.setProperty('--theme-text-colour', j99_themes[value].text_colour);
+                }
+            },
+            {
+                name: "Font size",
+                id: "fontsize",
+                type: "number",
+                min: 5,
+                default: 16,
+                update: function(value) {
+                    var css = document.documentElement.style;
+                    css.setProperty('--theme-font-size', `${value}px`);
+                }
+            },
+            {
+                name: "Page width",
+                id: "pagewidth",
+                type: "slider",
+                min: 5,
+                max: 100,
+                default: 90,
+                update: function(value) {
+                    var css = document.documentElement.style;
+                    css.setProperty('--theme-width', `${value}%`);
                 }
             }
         ];
@@ -132,6 +155,77 @@
                     options.appendChild(value_node);
                 }
                 dropdown.appendChild(options);
+            break;
+            case "number":
+                var numeric = document.createElement("input");
+                var attributes = {
+                    type: "number"
+                };
+                if (Object.hasOwn(setting, "min"))
+                    attributes["min"] = setting.min;
+                if (Object.hasOwn(setting, "max"))
+                    attributes["max"] = setting.max;
+                for (name in attributes)
+                {
+                    var attribute = document.createAttribute(name);
+                    attribute.value = attributes[name];
+                    numeric.attributes.setNamedItem(attribute);
+                }
+                numeric.value = j99_settings[setting.id];
+                numeric.onchange = (function(setting, numeric) {
+                    return function() {
+                        writeSetting(setting.id, numeric.value);
+                    }
+                })(setting, numeric);
+                settings_menu.appendChild(numeric);
+            break;
+            case "slider":
+                var slider = document.createElement("div");
+                slider.classList.add("slider");
+                settings_menu.appendChild(slider);
+                
+                var numeric = document.createElement("input");
+                var num_attributes = {
+                    type: "number",
+                    min: setting.min,
+                    max: setting.max
+                };
+                for (name in num_attributes)
+                {
+                    var attribute = document.createAttribute(name);
+                    attribute.value = num_attributes[name];
+                    numeric.attributes.setNamedItem(attribute);
+                }
+                numeric.value = j99_settings[setting.id];
+                
+                var actual_slider = document.createElement("input");
+                var as_attributes = {
+                    type: "range",
+                    min: setting.min,
+                    max: setting.max
+                };
+                for (name in as_attributes)
+                    {
+                        var attribute = document.createAttribute(name);
+                        attribute.value = as_attributes[name];
+                        actual_slider.attributes.setNamedItem(attribute);
+                    }
+                    actual_slider.value = j99_settings[setting.id];
+                    
+                    numeric.onchange = (function(setting, numeric, actual_slider) {
+                        return function() {
+                            actual_slider.value = numeric.value;
+                            writeSetting(setting.id, numeric.value);
+                        }
+                    })(setting, numeric, actual_slider);
+                    actual_slider.oninput = (function(numeric, actual_slider) {
+                        return function() {
+                        numeric.value = actual_slider.value;
+                        writeSetting(setting.id, actual_slider.value);
+                    }
+                })(numeric, actual_slider);
+                slider.appendChild(actual_slider);
+                slider.appendChild(numeric);
             break;
         }
     }
